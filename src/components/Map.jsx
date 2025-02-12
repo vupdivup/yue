85cbafac1d7e6b54239eb650421fe15d6d4f11ae
vupdivup <vnmatt2003@gmail.com>
2025-02-12 14:13:18 +0100

    create map cursor

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,72 @@
-import { HSVToHSL } from "../utils/colors";
+import { useEffect, useRef, useState } from "react";
 import "../styles/map.css";
+import { HSVToHSL } from "../utils/colors";
+import { Cursor } from "./Cursor";
+import { clamp } from "../utils/math";
 
-export function Map({hue}) {
+export function Map({hue, handleChange}) {
+    // TODO: have it as rgb or hex instead
     const hsl = HSVToHSL(hue, 100, 100);
 
+    const [s, setS] = useState(0);
+    const [v, setV] = useState(0);
+    const [coords, setCoords] = useState({});
+
+    const map = useRef(null);
+    const isMouseDown = useRef(false);
+
+    useEffect(() => {
+        window.addEventListener("mouseup", handleMouseUp);
+        window.addEventListener("mousemove", handleMouseMove)
+
+        return () => {
+            window.removeEventListener("mouseup", handleMouseUp);
+            window.removeEventListener("mousemove", handleMouseMove)
+        }
+    }, []);
+
+    function handleMouseDown(e) {
+        isMouseDown.current = true;
+    }
+
+    function handleMouseUp(e) {
+        isMouseDown.current = false;
+    }
+
+    function handleMouseMove(e) {
+        if (!isMouseDown.current) return;
+
+        const rect = map.current.getBoundingClientRect();
+
+        // TODO: hide cursor when dragging
+
+        const x = clamp(e.clientX - rect.x, 0, rect.width);
+        const y = clamp(e.clientY - rect.y, 0, rect.height);
+
+        setS(x / rect.width);
+        setV(y / rect.height);
+
+        setCoords({x: x, y: y});
+
+        //handleChange();
+    }
+
     return (
-        <div className="map">
+        <div
+            className="map"
+            ref={map}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+        >
             <div
                 className="map-hue"
                 style={
                     {background: `hsl(${hsl.h} ${hsl.s} ${hsl.l})`}
                 }
             />
-            <div className="map-gradient-s" />
-            <div className="map-gradient-v" />
+            <div className="map-gradient saturation" />
+            <div className="map-gradient value" />
+            <Cursor coords={coords}/>
         </div>
     );
 }
---
+++ src/utils/math.js
@@ -0,0 +1,5 @@
+export function clamp(num, min, max) {
+    if (num < min) return min;
+    if (num > max) return max;
+    return num;
+}
