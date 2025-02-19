896d5c55aeca959a1fbdf798be552638eef64c2f
vupdivup <vnmatt2003@gmail.com>
2025-02-19 11:54:39 +0100

    write hsv conversion functions

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,3 +1,5 @@
+import {roundObj} from "./math.js";
+
 export function HSVToHSL(h, s, v) {
     const hsl = {}
 
@@ -58,4 +60,69 @@ export function RGBToCMYK({r, g, b}) {
         y: Math.round(y * 100),
         k: Math.round(k * 100)
     }
+}
+
+export function HSVToRGB({h, s, v}) {
+    let rgb;
+
+    const c = (v * s) / 10000;
+    const h_ = h / 60;
+    const x = c * (1 - Math.abs(h_ % 2 - 1));
+
+    if (h_ < 1) {
+        rgb = {r: c, g: x, b: 0};
+    } else if (h_ < 2) {
+        rgb = {r: x, g: c, b: 0};
+    } else if (h_ < 3) {
+        rgb = {r: 0, g: c, b: x};
+    } else if (h_ < 4) {
+        rgb = {r: 0, g: x, b: c};
+    } else if (h_ < 5) {
+        rgb = {r: x, g: 0, b: c};
+    } else {
+        rgb = {r: c, g: 0, b: x};
+    }
+
+    const m = v / 100 - c;
+
+    rgb = {
+        r: (rgb.r + m) * 255,
+        g: (rgb.g + m) * 255,
+        b: (rgb.b + m) * 255
+    }
+
+    return roundObj(rgb);
+}
+
+export function RGBToHSV({r, g, b}) {
+    let hsv = {};
+
+    r = r / 255;
+    g = g / 255;
+    b = b / 255;
+
+    const xMax = Math.max(r, g, b);
+    const xMin = Math.min(r, g, b);
+
+    hsv.v = xMax;
+    
+    const c = xMax - xMin;
+
+    if (c === 0) {
+        hsv.h = 0;
+    } else if (hsv.v === r) {
+        hsv.h = ((g - b) / c) % 6;
+    } else if (hsv.v === g) {
+        hsv.h = (b - r) / c + 2;
+    } else {
+        hsv.h = (r - g) / c + 4;
+    }
+
+    hsv.h *= 60;
+
+    hsv.s = hsv.v === 0 ? 0 : c / hsv.v;
+
+    hsv = {...hsv, s: hsv.s * 100, v: hsv.v * 100};
+
+    return roundObj(hsv);
 }
--- src/utils/math.js
+++ src/utils/math.js
@@ -3,3 +3,14 @@ export function clamp(num, min, max) {
     if (num > max) return max;
     return num;
 }
+
+export function roundObj(o) {
+    // shallow copy
+    o = {...o};
+
+    for (const [key, value] of Object.entries(o)) {
+        o[key] = Math.round(value);
+    }
+
+    return o;
+}
